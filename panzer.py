48fd91b9b8e26c130436169428ffd3255d45dc11
Peter Butkovic <butkovic@gmail.com>
2022-09-26 21:48:35 +0200

    feat: green tank introduced

--- panzer.py
+++ panzer.py
@@ -1,8 +1,8 @@
 import pgzrun
 import random
 
-WIDTH =  1700
-HEIGHT =  950
+WIDTH = 1700
+HEIGHT = 950
 PANZER_STEP = 2
 SHOT_STEP = 2 * PANZER_STEP
 
@@ -16,6 +16,8 @@ panzerGreen = Actor('tanks_tank_green1_l')
 panzerGreen.bottomright = WIDTH, HEIGHT
 
 boxWooden = Actor('tanks_crate_wood')
+boxWooden.images = ['tanks_crate_wood', 'tank_explosion2','tank_explosion3','tank_explosion4']
+boxWooden.fps = 1
 boxWooden.topleft = random.randint(0, WIDTH), random.randint(0, HEIGHT)
 
 def draw():
@@ -23,11 +25,11 @@ def draw():
     panzerDesert.draw()
     shotDesert.draw()
     boxWooden.draw()
-#    panzerGreen.draw()
+    panzerGreen.draw()
 
 
 def handleShot():
-    if keyboard.SPACE:
+    if keyboard.space:
 
         if panzerDesert.image == 'tanks_tank_desert1_l':
             shotDesert.image = 'tank_bullet5_l'
@@ -44,64 +46,69 @@ def handleShot():
 
     if shotDesert.image == 'tank_bullet5_r':
         shotDesert.left = shotDesert.left + SHOT_STEP
-    if shotDesert.image == 'tank_bullet5_l':
+    elif shotDesert.image == 'tank_bullet5_l':
         shotDesert.left = shotDesert.left - SHOT_STEP
-    if shotDesert.image == 'tank_bullet5_u':
+    elif shotDesert.image == 'tank_bullet5_u':
         shotDesert.top = shotDesert.top - SHOT_STEP
-    if shotDesert.image == 'tank_bullet5_d':
+    elif shotDesert.image == 'tank_bullet5_d':
         shotDesert.top = shotDesert.top + SHOT_STEP
 
-def handlePazer():
-    if keyboard.left:
-        panzerDesert.image = 'tanks_tank_desert1_l'
-    elif keyboard.right:
-        panzerDesert.image = 'tanks_tank_desert1_r'
-    elif keyboard.up:
-        panzerDesert.image = 'tanks_tank_desert1_u'
-    elif keyboard.down:
-        panzerDesert.image = 'tanks_tank_desert1_d'
+    if shotDesert.colliderect(boxWooden):
+#        boxWooden.animate()
+        boxWooden.image = 'tank_explosion4'
+        #shotDesert.left = WIDTH
+        shotDesert.bottomleft = WIDTH + 100, HEIGHT + 100
+
+    elif boxWooden.image == 'tank_explosion4':
+        boxWooden.left = WIDTH
+
+def handlePazer(panzer, key_left, key_right, key_up, key_down, image_left, image_right, image_up, image_down):
+    if key_left:
+        panzer.image = image_left
+    elif key_right:
+        panzer.image = image_right
+    elif key_up:
+        panzer.image = image_up
+    elif key_down:
+        panzer.image = image_down
 
 
-    if panzerDesert.image == 'tanks_tank_desert1_r':
-        panzerDesert.left = panzerDesert.left + PANZER_STEP
-        if panzerDesert.right > WIDTH:
-            panzerDesert.image = 'tanks_tank_desert1_l'
+    if panzerDesert.image == image_right:
+        panzer.left = panzer.left + PANZER_STEP
+        if panzer.right > WIDTH:
+            panzer.image = image_left
 
-    elif panzerDesert.image == 'tanks_tank_desert1_l':
-        panzerDesert.left = panzerDesert.left - PANZER_STEP
-        if panzerDesert.left < 0:
-            panzerDesert.image = 'tanks_tank_desert1_r'
+    elif panzer.image == image_left:
+        panzer.left = panzer.left - PANZER_STEP
+        if panzer.left < 0:
+            panzer.image = image_right
 
-    elif panzerDesert.image == 'tanks_tank_desert1_u':
-        panzerDesert.top = panzerDesert.top - PANZER_STEP
-        if panzerDesert.top < 0:
-            panzerDesert.image = 'tanks_tank_desert1_d'
+    elif panzer.image == image_up:
+        panzer.top = panzer.top - PANZER_STEP
+        if panzer.top < 0:
+            panzer.image = image_down
 
-    elif panzerDesert.image == 'tanks_tank_desert1_d':
-        panzerDesert.bottom = panzerDesert.bottom + PANZER_STEP
-        if panzerDesert.bottom > HEIGHT:
-            panzerDesert.image = 'tanks_tank_desert1_u'
+    elif panzer.image == image_down:
+        panzer.bottom = panzer.bottom + PANZER_STEP
+        if panzer.bottom > HEIGHT:
+            panzer.image = image_up
 
-    if panzerDesert.colliderect(boxWooden):
-        if panzerDesert.image == 'tanks_tank_desert1_r':
-            panzerDesert.image = 'tanks_tank_desert1_l'
-        elif panzerDesert.image == 'tanks_tank_desert1_l':
-            panzerDesert.image = 'tanks_tank_desert1_r'
-        elif panzerDesert.image == 'tanks_tank_desert1_u':
-            panzerDesert.image = 'tanks_tank_desert1_d'
-        elif panzerDesert.image == 'tanks_tank_desert1_d':
-            panzerDesert.image = 'tanks_tank_desert1_u'
+    if panzer.colliderect(boxWooden):
+        if panzer.image == image_right:
+            panzer.image = image_left
+        elif panzer.image == image_left:
+            panzer.image = image_right
+        elif panzer.image == image_up:
+            panzer.image = image_down
+        elif panzer.image == image_down:
+            panzer.image = image_up
 
 
 
 def update():
 
     handleShot()
-    handlePazer()
-
-
-#    panzerGreen.left -= 2
-#    if panzerGreen.left < 0:
-#        panzerGreen.right = 0
+    handlePazer(panzerDesert, keyboard.left, keyboard.right, keyboard.up, keyboard.down, 'tanks_tank_desert1_l', 'tanks_tank_desert1_r', 'tanks_tank_desert1_u', 'tanks_tank_desert1_d')
+    handlePazer(panzerGreen, keyboard.a, keyboard.d, keyboard.w, keyboard.s, 'tanks_tank_green1_l', 'tanks_tank_green1_r', 'tanks_tank_green1_u', 'tanks_tank_green1_d')
 
 pgzrun.go()
