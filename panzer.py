aec43d5f4c8fb8fc9f9e684221e983b92eef566a
Peter Butkovic <butkovic@gmail.com>
2022-10-02 15:27:04 +0200

    feat: walls, background, tanks moving

--- panzer.py
+++ panzer.py
@@ -1,48 +1,75 @@
 import pgzrun
 import random
 
-WIDTH = 1700
-HEIGHT = 950
+WIDTH = 800 #1700
+HEIGHT = 600 #950
 PANZER_STEP = 2
 SHOT_STEP = 2 * PANZER_STEP
 
-panzerDesert = Actor('tanks_tank_desert1_r')
-panzerDesert.topleft = 0, 10
+tankBlue = Actor('tank_blue')
+tankBlue.topleft = 0, 10
 
+grass = []
+walls = []
+
+shots = []
 shotDesert = Actor('tank_bullet5_l')
 shotDesert.topright = 0, 10
+shots.append(shotDesert)
 
-panzerGreen = Actor('tanks_tank_green1_l')
-panzerGreen.bottomright = WIDTH, HEIGHT
+tankGreen = Actor('tank_green')
+tankGreen.bottomright = WIDTH, HEIGHT
 
-boxWooden = Actor('tanks_crate_wood')
-boxWooden.images = ['tanks_crate_wood', 'tank_explosion2','tank_explosion3','tank_explosion4']
-boxWooden.fps = 1
-boxWooden.topleft = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+def generateGrass():
+    for x in range(int(WIDTH/64) + 2):
+        for y in range(int(HEIGHT/64) + 2):
+            grass1 = Actor('tile_grass1')
+            grass1.x = 64 * x
+            grass1.y = 64 * y
+            grass.append(grass1)
+
+
+def generateWalls():
+    for x in range(int(WIDTH/28) + 1):
+        for y in range(int(HEIGHT/28) + 1):
+            if random.randint(0, 100) < 10 and y > 2 and y < HEIGHT/28 - 3:
+                wall = Actor('crate_metal')
+                wall.topleft = (28 * x, 28 * y)
+                walls.append(wall)
+
+generateGrass()
+generateWalls()
 
 def draw():
     screen.clear()
-    panzerDesert.draw()
-    shotDesert.draw()
-    boxWooden.draw()
-    panzerGreen.draw()
+    #screen.fill((0,0,0))
+    #background.draw()
 
+
+    for grass1 in grass:
+        grass1.draw()
+    for wall in walls:
+        wall.draw()
+
+    tankBlue.draw()
+    shotDesert.draw()
+    tankGreen.draw()
 
 def handleShot():
     if keyboard.space:
 
-        if panzerDesert.image == 'tanks_tank_desert1_l':
+        if tankBlue.image == 'tanks_tank_desert1_l':
             shotDesert.image = 'tank_bullet5_l'
-            shotDesert.topright = panzerDesert.topleft
-        elif panzerDesert.image == 'tanks_tank_desert1_r':
+            shotDesert.topright = tankBlue.topleft
+        elif tankBlue.image == 'tanks_tank_desert1_r':
             shotDesert.image = 'tank_bullet5_r'
-            shotDesert.topleft = panzerDesert.topright
-        elif panzerDesert.image == 'tanks_tank_desert1_u':
+            shotDesert.topleft = tankBlue.topright
+        elif tankBlue.image == 'tanks_tank_desert1_u':
             shotDesert.image = 'tank_bullet5_u'
-            shotDesert.bottomleft = panzerDesert.topleft
-        elif panzerDesert.image == 'tanks_tank_desert1_d':
+            shotDesert.bottomleft = tankBlue.topleft
+        elif tankBlue.image == 'tanks_tank_desert1_d':
             shotDesert.image = 'tank_bullet5_d'
-            shotDesert.topright = panzerDesert.bottomright
+            shotDesert.topright = tankBlue.bottomright
 
     if shotDesert.image == 'tank_bullet5_r':
         shotDesert.left = shotDesert.left + SHOT_STEP
@@ -53,62 +80,40 @@ def handleShot():
     elif shotDesert.image == 'tank_bullet5_d':
         shotDesert.top = shotDesert.top + SHOT_STEP
 
-    if shotDesert.colliderect(boxWooden):
-#        boxWooden.animate()
-        boxWooden.image = 'tank_explosion4'
-        #shotDesert.left = WIDTH
-        shotDesert.bottomleft = WIDTH + 100, HEIGHT + 100
+def handlePazer(tank, key_left, key_right, key_up, key_down, image):
 
-    elif boxWooden.image == 'tank_explosion4':
-        boxWooden.left = WIDTH
+    # Save the original position of the tank
+    original_x = tank.x
+    original_y = tank.y
 
-def handlePazer(panzer, key_left, key_right, key_up, key_down, image_left, image_right, image_up, image_down):
     if key_left:
-        panzer.image = image_left
+        tank.x = tank.x - 2
+        tank.angle = 270
     elif key_right:
-        panzer.image = image_right
+        tank.x = tank.x + 2
+        tank.angle = 90
     elif key_up:
-        panzer.image = image_up
+        tank.y = tank.y - 2
+        tank.angle = 180
     elif key_down:
-        panzer.image = image_down
+        tank.y = tank.y + 2
+        tank.angle = 0
 
+    # Return tank to original position if colliding with wall
+    if tank.collidelist(walls) != -1:
+        tank.x = original_x
+        tank.y = original_y
 
-    if panzerDesert.image == image_right:
-        panzer.left = panzer.left + PANZER_STEP
-        if panzer.right > WIDTH:
-            panzer.image = image_left
-
-    elif panzer.image == image_left:
-        panzer.left = panzer.left - PANZER_STEP
-        if panzer.left < 0:
-            panzer.image = image_right
-
-    elif panzer.image == image_up:
-        panzer.top = panzer.top - PANZER_STEP
-        if panzer.top < 0:
-            panzer.image = image_down
-
-    elif panzer.image == image_down:
-        panzer.bottom = panzer.bottom + PANZER_STEP
-        if panzer.bottom > HEIGHT:
-            panzer.image = image_up
-
-    if panzer.colliderect(boxWooden):
-        if panzer.image == image_right:
-            panzer.image = image_left
-        elif panzer.image == image_left:
-            panzer.image = image_right
-        elif panzer.image == image_up:
-            panzer.image = image_down
-        elif panzer.image == image_down:
-            panzer.image = image_up
+    if tank.x < 0 or tank.x > WIDTH or tank.y < 0 or tank.y > HEIGHT:
+        tank.x = original_x
+        tank.y = original_y
 
 
 
 def update():
 
     handleShot()
-    handlePazer(panzerDesert, keyboard.left, keyboard.right, keyboard.up, keyboard.down, 'tanks_tank_desert1_l', 'tanks_tank_desert1_r', 'tanks_tank_desert1_u', 'tanks_tank_desert1_d')
-    handlePazer(panzerGreen, keyboard.a, keyboard.d, keyboard.w, keyboard.s, 'tanks_tank_green1_l', 'tanks_tank_green1_r', 'tanks_tank_green1_u', 'tanks_tank_green1_d')
+    handlePazer(tankBlue, keyboard.left, keyboard.right, keyboard.up, keyboard.down, 'tank_blue')
+    handlePazer(tankGreen, keyboard.a, keyboard.d, keyboard.w, keyboard.s, 'tank_green')
 
 pgzrun.go()
